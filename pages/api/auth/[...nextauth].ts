d7ba2c32e7dcecdc45ccf53b345e52b37ce5f2ef
yuanyuan <yuanyuan799@nuaa.edu.cn>
2025-03-10 23:35:45 +0800

    修改

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,10 +13,6 @@ export const authOptions: AuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
-      httpOptions: {
-        timeout: 10000, // 设置超时时间为 10秒
-      },
-      
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
